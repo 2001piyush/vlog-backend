8f103da8a168b2b79d303697951f47604ecb3b69
piyush <131507943+2001piyush@users.noreply.github.com>
2025-05-11 12:11:49 +0530

    Update auth.js
    session field is changed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 export const isAuthenticated = (req, res, next) => {
-    if (req.session.userId) {
+    if (req.session.adminUser ) {
         next();
     } else {
         res.status(401).json({ message: 'Not authenticated' });
